f947341f824e518bc0642b43162a54cd27973948
Daniel <42862105+9hitste@users.noreply.github.com>
2022-05-21 08:53:05 +0700

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -238,12 +238,12 @@ EOL
 function install_apt() {
 	DEBIAN_FRONTEND=noninteractive apt-get update
 	DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-	DEBIAN_FRONTEND=noninteractive apt-get install -y xvfb bzip2 libcanberra-gtk-module libxss1 htop sed tar libxtst6 libnss3 wget psmisc bc libgtk-3-0 libgbm-dev libatspi2.0-0 libatomic1 && install_9hits
+	DEBIAN_FRONTEND=noninteractive apt-get install -y cron xvfb bzip2 libcanberra-gtk-module libxss1 htop sed tar libxtst6 libnss3 wget psmisc bc libgtk-3-0 libgbm-dev libatspi2.0-0 libatomic1 && install_9hits
 }
 
 function install_yum() {
 	yum update -y
-	yum install -y libatomic alsa-lib-devel gtk3-devel libgbm libxkbcommon-x11 cups-libs.i686 cups-libs.x86_64 atk.x86_64 libnss3.so xorg-x11-server-Xvfb sed tar Xvfb wget bzip2 libXScrnSaver psmisc && install_9hits
+	yum install -y cronie libatomic alsa-lib-devel gtk3-devel libgbm libxkbcommon-x11 cups-libs.i686 cups-libs.x86_64 atk.x86_64 libnss3.so xorg-x11-server-Xvfb sed tar Xvfb wget bzip2 libXScrnSaver psmisc && install_9hits
 }
 
 function check_dist() {

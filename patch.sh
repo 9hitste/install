688891aaa394b1d7e065173b440a5432cf545f4e
Daniel <42862105+9hitste@users.noreply.github.com>
2024-07-10 09:29:02 +0700

    Delete patch.sh

--- patch.sh
+++
@@ -1,6 +0,0 @@
-_9HITSUSER="_9hits"
-ARGS=$@
-wget -O "/tmp/get-patch.sh" https://9hitste.github.io/install/get-patch.sh
-chmod +x "/tmp/get-patch.sh"
-runuser -l $_9HITSUSER -c "/bin/bash /tmp/get-patch.sh $(printf "%q" "$ARGS")"
-rm -f "/tmp/get-patch.sh"
